445f8ee859d061db7f7d26c92c72912f515fa772
aAlejandroZ <azalejandro01@gmail.com>
2019-08-26 13:41:01 -0500

    Fooling around with middleware

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = 4000;
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
+app.use(express.json({extended: true}));
 
 app.listen(PORT, function(){
     console.log('Server is running on PORT:' + PORT)
