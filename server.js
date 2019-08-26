eac1460a46a8fb3604e10db22c9e424460ac1378
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-08-26 14:16:58 -0500

    Added keys file and additional dependencies

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 const PORT = 4000;
-
 
 // mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true});
 // const connection = mongoose.connection;
@@ -14,9 +13,18 @@ const PORT = 4000;
 // });
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 
-app.listen(PORT, function(){
-    console.log('Server is running on PORT:' + PORT)
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+app.listen(PORT, function() {
+  console.log(`Server is running on PORT: ${PORT}`);
 });
