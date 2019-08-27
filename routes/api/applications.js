55cc6a7a0faa3e237ba16790b5ace633005ef436
aAlejandroZ <azalejandro01@gmail.com>
2019-08-26 23:23:13 -0500

    Made promises into async functions

--- routes/api/applications.js
+++ routes/api/applications.js
@@ -8,6 +8,13 @@ const Application = require("../../models/application.model");
 // @desc    Get all applications
 // @access  Public
 router.get("/", (req, res) => {
+  // try{
+  //   const app = await Application.find();
+  //   app.sort({companyName: 1})
+  //   res.json(app);
+  // }catch(err){
+  //   console.log(err);
+  // }
   Application.find()
     .sort({ companyName: 1 }) // Sorts company name in alphabetical order
     .then(app => res.json(app));
@@ -16,45 +23,71 @@ router.get("/", (req, res) => {
 // @route   GET api/applications/:id
 // @desc    Get an application by id
 // @access  Public
-router.get("/:id", (req, res) => {
-  Application.findById(req.params.id).then(app => res.json(app));
+router.get("/:id", async (req, res) => {
+  try{
+    const app = await Application.findById(req.params.id);
+    res.json(app);
+  }catch(err){
+    console.log(err);
+  }
 });
 
 // @route   POST api/applications
 // @desc    Create an application
 // @access  Public
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // Create new application by taking data from the json request
-  const newApplication = new Application({
-    companyName: req.body.companyName,
-    applicationDate: req.body.applicationDate,
-    position: req.body.position,
-    location: req.body.location,
-    salary: req.body.salary,
-    status: req.body.status,
-    portalLink: req.body.portalLink
-  });
-
-  // Save new application to database
-  newApplication.save().then(app => res.json(app));
+  try{
+    const newApplication = new Application({
+      companyName: req.body.companyName,
+      applicationDate: req.body.applicationDate,
+      position: req.body.position,
+      location: req.body.location,
+      salary: req.body.salary,
+      status: req.body.status,
+      portalLink: req.body.portalLink
+    });
+    
+    res.json(await newApplication.save());  // Save new application to database
+    }catch (err){
+        console.log(err);
+  }
 });
 
 // @route   UPDATE api/applications/:id
 // @desc    UPDATE an application by id
 // @access  Public
-router.put("/:id", (req, res) => {
-  Application.findById(req.params.id)
-    .then(app => app.update(req.body).then(data => res.json("Application updated")))  // Updates application with the fields passed in the request body
-    .catch(err => res.status(404).json("Could not find application with that ID"));   // If no application found with given id, return error
+router.put("/:id", async (req, res) => {
+  try{
+    const app = await Application.findById(req.params.id);
+    if (app == null){
+      console.log("Application not inside database")
+    }else{
+      app.update(req.body);
+      res.json("Application has been updated")
+    }
+  }catch(err){
+    res.status(404).json("Could not find application with that ID")
+  }
+  // Application.findById(req.params.id)
+  //   .then(app => app.update(req.body).then(data => res.json("Application updated")))  // Updates application with the fields passed in the request body
+  //   .catch(err => res.status(404).json("Could not find application with that ID"));   // If no application found with given id, return error
 });
 
 // @route   DELETE api/applications/:id
 // @desc    Delete an application by id
 // @access  Public
-router.delete("/:id", (req, res) => {
-  Application.findById(req.params.id)
-    .then(app => app.remove().then(() => res.json({ app, success: true })))   // Removes application by id
-    .catch(err => res.status(404).json({ success: false }));                  // If no application found with given id, return error
+router.delete("/:id", async (req, res) => {
+  try{
+    const app = await Application.findById(req.params.id)
+    if (app == null){ // If no application found with given id, return error
+      console.log("Application not inside database")
+      res.json({"success": false})
+  }else // If app is found, delete it from the database
+      res.json(await app.delete())
+  }catch(err){  
+    console.log(err);
+  }                
 });
 
 module.exports = router;
