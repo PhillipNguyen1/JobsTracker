7e1fdf74c50c273c4c06bf2728c3144ffd662abb
aAlejandroZ <azalejandro01@gmail.com>
2019-09-04 22:34:52 -0500

    Finished simple Navbar, but my primarly goal is to work on the AppTab

--- client/src/components/AppTab.js
+++ client/src/components/AppTab.js
@@ -1,21 +1,50 @@
-import React, {Component} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-import Typography from "@material-ui/core/Typography"
+import React from 'react';
 
 
-const Navbar = () =>{
+const AppTab = () =>{
+
+
     return(
         <div>
-            <AppBar position="static">
-                <ToolBar>
-                    <Typography variant='title' color='inherit'>
-                        React & Material UI Sample Yaba 
-                    </Typography>
-                </ToolBar>
-            </AppBar>
+            <h1>Hello World!</h1>
         </div>
     );
 }
 
-export default Navbar;
+export default AppTab;
+
+// import React from 'react';
+// import AppBar from '@material-ui/core/AppBar';
+// import { makeStyles } from '@material-ui/core/styles';
+// import ToolBar from '@material-ui/core/Toolbar';
+// import Typography from "@material-ui/core/Typography"
+// import Button from '@material-ui/core/Button';
+
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//         flexGrow: 1,
+//       },
+//       title: {
+//         flexGrow: 1,
+//       },
+// }));
+
+// const Navbar = () =>{
+//     const classes = useStyles();
+//     return(
+//         <div className={classes.root}>
+//             <AppBar position="static">
+//                 <ToolBar>
+//                     <Typography variant='title' color='inherit' className={classes.title}>
+//                         React & Material UI Sample Yaba 
+//                     </Typography>
+
+                    
+//                     <Button color="inherit">Login</Button>
+//                 </ToolBar>
+//             </AppBar>
+//         </div>
+//     );
+// }
+
+// export default Navbar;
