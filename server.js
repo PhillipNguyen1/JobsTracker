e415b313a664683aad1426084feac382c8393e11
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-10-21 21:35:21 -0500

    Added back server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 4000;
+
+const applications = require('./routes/api/applications');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
+const db = require("config").get('mongoURI')
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log(`Connected to database ${db}`))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/applications', applications);
+
+
+app.listen(PORT, function() {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
