cedc7cf64e2f2a5716982707bf47a3357e7e70ab
aAlejandroZ <azalejandro01@gmail.com>
2019-10-28 22:53:57 -0500

    Removed useless code, because Phillip wrote it

--- client/src/components/ApplicationsTable.js
+++ client/src/components/ApplicationsTable.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Button from "@material-ui/core/Button";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -28,7 +27,6 @@ const ApplicationsTable = () => {
 
   // applications is an array of objects
   const [applications, setApplications] = useState([]);
-  const [isVisible, setIsVisible] = useState(false); // list is initial invisible
 
   // This async function gets called whenever the page loads and will update the data accordingly
   async function fetchData() {
@@ -38,53 +36,21 @@ const ApplicationsTable = () => {
     } catch (err) {
       console.error(err);
     }
-    // setApplications(result.data);
-    console.log(result.data);
   }
-
   useEffect(() => {
     fetchData();
   }, []);
 
-  // Adds an application to the hook state
-  const handleNewApplication = event => {
-    console.log("Adding Application...");
-
-    // Create a new Application
-    const newApplication = {
-      id: applications.length + 1,
-      companyName: "Example",
-      position: "software engineer"
-    };
-
-    // Append the new application to the end of the list
-    setApplications([...applications, newApplication]);
-
-    // Click on 'List Applications in console' button to see new hook state
-  };
-
-  // lists applications in console
-  const handleListApplications = event => {
-    console.log("Listing Applications...");
-    setIsVisible(!isVisible); // toggles between true & false
-  };
-
-  // clears the list of application
-  const handleClear = event => {
-    console.log("Clearing Applications...");
-    setApplications([]);
-  };
-
-
   // List of header
-  const headers = ["Position", "Application Data", "Status", "Response", "How Far", "Portal Link", "Job Board"];
-
-  // returns the list of application
-  const listApplications = (
-    <div className={classes.appList}>
-      
-    </div>
-  );
+  const headers = [
+    "Position",
+    "Application Data",
+    "Status",
+    "Response",
+    "How Far",
+    "Portal Link",
+    "Job Board"
+  ];
 
   return (
     <div>
@@ -95,7 +61,9 @@ const ApplicationsTable = () => {
               <TableCell>Company</TableCell>
               {/* Iterates through the headers array to create headers */}
               {headers.map(header => (
-                <TableCell key={header} align="right">{header}</TableCell>
+                <TableCell key={header} align="right">
+                  {header}
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
@@ -103,7 +71,9 @@ const ApplicationsTable = () => {
           <TableBody>
             {applications.map(app => (
               <TableRow key={app.companyName}>
-                <TableCell component="th" scope="row">{app.companyName}</TableCell>
+                <TableCell component="th" scope="row">
+                  {app.companyName}
+                </TableCell>
                 <TableCell align="right">{app.position}</TableCell>
                 <TableCell align="right">{app.applicationDate}</TableCell>
                 <TableCell align="right">{app.status}</TableCell>
