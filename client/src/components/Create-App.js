0e9128f90ff23bf2bc53d5b538ca1d394f83355f
aAlejandroZ <azalejandro01@gmail.com>
2019-10-29 23:09:32 -0500

    Finished connecting creation of app to database

--- client/src/components/Create-App.js
+++ client/src/components/Create-App.js
@@ -30,7 +30,6 @@ const useStyles = makeStyles(theme => ({
 export default function CreateApplication() {
   const classes = useStyles();
 
-
   const [formState, setformState] = useState({
     companyName: "",
     position: "",
@@ -43,92 +42,159 @@ export default function CreateApplication() {
     salary: ""
   });
 
-  function handleSubmit (event){
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log(event)
+
+    axios.post("http://localhost:4000/api/applications", formState).then(res => {
+      console.log(res.data);
+    });
   }
 
   return (
-    <div style={{ padding: 16, margin: 'auto', maxWidth: 1500 }}>
-      
-    <form
-      className={classes.container}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
-      {/* <input name="ASDA" ref={register}/> */}
-      <TextField
-        required
-        id="standard-name"
-        label="Company Name"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        required
-        id="standard-uncontrolled"
-        label="Position"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        required
-        id="standard-required"
-        label="Aplpication Date"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="standard-uncontrolled"
-        label="Status"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="standard-uncontrolled"
-        label="Response?"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="standard-uncontrolled"
-        label="How Far?"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="standard-read-only-input"
-        label="Portal Link"
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="standard-dense"
-        label="Job Board"
-        className={clsx(classes.textField, classes.dense)}
-        margin="dense"
-      />
-      <TextField
-        id="standard-number"
-        label="Salary"
-        type="number"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true
-        }}
-        margin="normal"
-      />
-
-      <Button 
-      variant="contained"
-      color="primary"
-      type="submit"
-      className={classes.button}
+    <div style={{ padding: 16, margin: "auto", maxWidth: 1500 }}>
+      <form
+        className={classes.container}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
       >
-      Submit
-      </Button>
-    </form>
+        <TextField
+          required
+          id="standard-name"
+          name="companyName"
+          label="Company Name"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          className={classes.textField}
+          margin="normal"
+        />
+        <TextField
+          required
+          id="standard-uncontrolled"
+          label="Position"
+          className={classes.textField}
+          name="position"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+        />
+        <TextField
+          required
+          id="standard-required"
+          label="Aplpication Date"
+          className={classes.textField}
+          name="applicationDate"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+        />
+        <TextField
+          id="standard-uncontrolled"
+          label="Status"
+          className={classes.textField}
+          name="status"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+        />
+        <TextField
+          id="standard-uncontrolled"
+          label="Response?"
+          className={classes.textField}
+          name="response"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+        />
+        <TextField
+          id="standard-uncontrolled"
+          label="How Far?"
+          name="howFar"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+          className={classes.textField}
+          margin="normal"
+        />
+        <TextField
+          id="standard-read-only-input"
+          label="Portal Link"
+          name="portalLink"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+          className={classes.textField}
+          margin="normal"
+        />
+        <TextField
+          id="standard-dense"
+          label="Job Board"
+          name="jobBoard"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          margin="normal"
+          className={clsx(classes.textField, classes.dense)}
+          margin="dense"
+        />
+        <TextField
+          id="standard-number"
+          label="Salary"
+          type="number"
+          name="salary"
+          onChange={event =>
+            setformState({
+              ...formState,
+              [event.target.name]: event.target.value
+            })
+          }
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true
+          }}
+          margin="normal"
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className={classes.button}
+        >
+          Submit
+        </Button>
+      </form>
     </div>
   );
 }
